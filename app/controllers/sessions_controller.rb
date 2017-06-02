15412334fda309c85ab9bf9780a04f4eb0fd45d5
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-02 10:16:05 -0400

    added user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to episodes_path
+      redirect_to user_path(user)
     else
-      redirect_to login_path
+      redirect_to episodes_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'episodes#index'
   delete '/logout' => 'sessions#destroy', as: 'logout'
   resources :sessions, only: :create
   resources :guests, only: [:index, :show]
   resources :episodes, only: [:index, :show]
+  resources :users, only: :show
   resources :appearances, only: [:new, :create, :edit, :update]
 
 end
